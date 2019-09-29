5596a5416af34d3a1fda8e05e3839c93e3b819e2
Aaron <aaronlegomaster370@gmail.com>
2019-09-29 15:36:23 -0700

    more

---
+++ ECMA6/variables/const.js
@@ -0,0 +1,6 @@
+const a = 2;
+
+a = a;
+//a = 1; //Assignment to constant variable.
+
+console.log(a);
---
+++ ECMA6/variables/let.js
@@ -0,0 +1,10 @@
+function test() {
+
+  //let a = 1;
+
+  //let a = "2";
+  //will result in an error
+  console.log(a);
+}
+
+test();
